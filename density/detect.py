218ec841045cf8f5f18a206715f962c5c17e5ad7
Anmol <anmolkrjha006@gmail.com>
2025-10-10 23:41:01 +0530

    Debugged the previous commit

--- density/detect.py
+++ density/detect.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-from ultralytics import YOLO
-model = YOLO('yolov8n.pt')
-
-source = 'sample.mp4' 
-
-results = model.predict(source, show=True, save=True, conf=0.5)
-print("Detection complete! The output video is saved in the 'runs' folder.")
-=======
 import cv2
 import threading
 from ultralytics import YOLO
@@ -106,4 +97,4 @@ if __name__ == "__main__":
         print("No results were calculated.")
     print("---------------------------------------\n")
     print("All processing is complete.")
->>>>>>> anmol
+
